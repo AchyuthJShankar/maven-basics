7379fc0c01bcd11f23b2dc941cc8cf8b4449b23b
Achyuth <achyuththeajs@gmail.com>
2022-09-21 14:31:27 +0530

    modified

--- src/test/java/seleniumexamples/AlertBank.java
+++ src/test/java/seleniumexamples/AlertBank.java
@@ -1,5 +1,7 @@
 package seleniumexamples;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,18 +12,17 @@ import org.testng.Assert;
 
 
 public class AlertBank {
-	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\Achyuth\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		//  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("https://netbanking.hdfcbank.com/netbanking/");
 		driver.switchTo().frame("login_page");
 		driver.findElement(By.xpath("//a[contains(text(),'CONTINUE')]")).click();
 		Alert HdfcAlert=driver.switchTo().alert();
 		String actualResult=HdfcAlert.getText();
-		Thread.sleep(1000);
+		//Thread.sleep(1000);
+		driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
 		HdfcAlert.accept();
 		//HdfcAlert.dismiss();
 		driver.switchTo().defaultContent();// This method will take back to main window mean comes out of the login frame.
